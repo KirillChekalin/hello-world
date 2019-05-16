28225c446adcf4468c98e14a9b8e27f3df320488
KirillChekalin <44983843+KirillChekalin@users.noreply.github.com>
2019-05-16 12:25:34 +0300

    Update app.js

--- test/app.js
+++ test/app.js
@@ -4,6 +4,6 @@ let server = new http.Server((req, res) => {
   res.end('hello world');
 });
 
-server.listen(1337, '127.0.0.1', () => {
+server.listen(3000, '172.31.80.253', () => {
   console.log('server listen');
 });
